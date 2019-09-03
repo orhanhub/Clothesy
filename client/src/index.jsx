20a4f9e80b46049eb71b8e2edeb62c48d9daf329
Jared A Sutton <jaredasutton@Jareds-MacBook-Pro.local>
2019-09-03 19:12:25 -0400

    Fix path reference to store folder.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
 const React = require("react");
 const { render } = require("react-dom");
 const { Provider } = require("react-redux");
-const store = require("./store");
+const store = require("../../store");
 
 let App = () => {
   return <div>Hello, world.</div>;
