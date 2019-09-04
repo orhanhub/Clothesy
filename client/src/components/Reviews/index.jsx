3532346c1b3d8e1984d604895bc8d9bf35b27533
Jared A Sutton <jaredasutton@Jareds-MacBook-Pro.local>
2019-09-04 16:37:20 -0400

    Fix bar-and-marker and starfill css properties for Firefox.

--- client/src/components/Reviews/index.jsx
+++ client/src/components/Reviews/index.jsx
@@ -2,6 +2,7 @@ const React = require("react");
 const { Paper, Typography } = require("@material-ui/core");
 const { makeStyles } = require("@material-ui/core/styles");
 const MarkerBar = require("../shared/MarkerBar.jsx");
+const StarFill = require("../shared/StarFill.jsx");
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -15,6 +16,7 @@ module.exports = props => {
     <Paper className={classes.root}>
       <Typography>Hello, revieworld.</Typography>
       <MarkerBar percentage={25} />
+      <StarFill percentage={67.5} />
     </Paper>
   );
 };
