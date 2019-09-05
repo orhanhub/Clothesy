43dfd2302b6631deedb3de62371001421b1240bf
Orhan Yarar <orhan@hotmail.ca>
2019-09-04 20:31:02 -0400

    Search rendered but not functional

--- client/src/components/Qna/index.jsx
+++ client/src/components/Qna/index.jsx
@@ -1,14 +1,18 @@
 const React = require("react");
-const { makeStyles } = require("@material-ui/core");
 const { Container, Typography, Grid, Paper } = require("@material-ui/core");
 const { QnaOneWrap } = require("./singleqna.jsx");
+const { SearchQnaButton } = require("./searchqnabutton.jsx");
 
 module.exports = props => {
   return (
     <div className="qnaComponentWrapper">
       <Container maxWidth="lg">
         <div>Search Bar</div>
+        <SearchQnaButton />
+        <div>end</div>
         <div className="qnaListWrapper">
+          <QnaOneWrap />
+          <QnaOneWrap />
           <QnaOneWrap />
         </div>
         <button>More Answered Questions</button>
