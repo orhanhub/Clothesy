dc3423fbc1be8b2b469282e7644a17bb2e4a3538
Jared A Sutton <jaredasutton@Jareds-MacBook-Pro.local>
2019-09-05 17:17:56 -0400

    Remove jQuery and console.log invocations.

--- client/src/components/shared/StarFill.jsx
+++ client/src/components/shared/StarFill.jsx
@@ -2,9 +2,7 @@ const React = require("react");
 const starsToPct = require("./helpers/starsToPct.js");
 
 module.exports = ({ stars }) => {
-  console.log(stars);
   let percentage = starsToPct(stars);
-  console.log(percentage);
   return (
     <div className="stars">
       <div className="empty-stars">☆☆☆☆☆</div>
