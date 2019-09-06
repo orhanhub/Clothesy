b9dfc24ad4d8c0ff4a0ebf8fa0238cf4bb7c7e3e
Jared A Sutton <jaredasutton@Jareds-MacBook-Pro.local>
2019-09-06 16:07:09 -0400

    Refactor store for limited prop distribution.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,11 @@ const React = require("react");
 const { Provider } = require("react-redux");
 const store = require("../../store");
 const OmniContainer = require("./containers/OmniContainer.jsx");
-const Overview = require("./containers/OverviewContainer.js");
-const Qna = require("./containers/QnaContainer.js");
-const Related = require("./containers/RelatedContainer.js");
-const Reviews = require("./containers/ReviewsContainer.js");
 
 let App = () => {
   return (
     <Provider store={store}>
-      <OmniContainer>
-        <Overview />
-        <Qna />
-        <Related />
-        <Reviews />
-      </OmniContainer>
+      <OmniContainer></OmniContainer>
     </Provider>
   );
 };
