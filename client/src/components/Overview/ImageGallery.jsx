c6f9035ba143114a1485298e0aee88ef666cf1e2
Eric Luu <ericluu24@gmail.com>
2019-09-05 20:07:57 -0400

    Moved imageGalleryList out of the imageGallery and fixed sliding overflow

--- client/src/components/Overview/ImageGallery.jsx
+++ client/src/components/Overview/ImageGallery.jsx
@@ -5,33 +5,34 @@ const ImageGallery = props => {
   const cropImage = {
     width: "775px",
     height: "550px",
-    objectFit: "cover",
-    padding: "10px"
+    objectFit: "cover"
   };
   return (
-    <Carousel style={cropImage}>
-      <Carousel.Item>
-        <img
-          style={cropImage}
-          src="https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
-          alt="First slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          style={cropImage}
-          src="https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80"
-          alt="Third slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          style={cropImage}
-          src="https://images.unsplash.com/photo-1549831243-a69a0b3d39e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2775&q=80"
-          alt="Third slide"
-        />
-      </Carousel.Item>
-    </Carousel>
+    <div style={{ overflow: "hidden", marginTop: "20px", width: "775px" }}>
+      <Carousel wrap={true}>
+        <Carousel.Item>
+          <img
+            style={cropImage}
+            src="https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            style={cropImage}
+            src="https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            style={cropImage}
+            src="https://images.unsplash.com/photo-1549831243-a69a0b3d39e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2775&q=80"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 };
 
