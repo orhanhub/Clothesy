0f7f181bba54e9a2a0d41d2334dc996a4dddc7ff
Jared A Sutton <jaredasutton@Jareds-MacBook-Pro.local>
2019-09-10 15:10:46 -0400

    Integrate filestack upload.

--- client/src/components/Reviews/AddReview.jsx
+++ client/src/components/Reviews/AddReview.jsx
@@ -5,16 +5,20 @@ const {
   Typography,
   RadioGroup,
   FormControlLabel,
-  Radio
+  Radio,
+  Button
 } = require("@material-ui/core");
 const { makeStyles } = require("@material-ui/core/styles");
 
 const AddReviewModal = require("../shared/Modal.jsx");
 const QualityRadioSelect = require("./QualityRadioSelect.jsx");
 const OverallRatingSelect = require("./OverallRatingSelect.jsx");
+const UploadThumbnails = require("./UploadThumbnails.jsx");
 const characteristics = require("./constants/characteristics.js");
+const fileUpload = require("../shared/fileUpload.js");
 
 module.exports = props => {
+  let [thumbnails, setThumbnails] = useState([]);
   return (
     <Grid item xs={12}>
       <AddReviewModal
@@ -29,6 +33,14 @@ module.exports = props => {
             characteristic={{ name: char, levels: ratings }}
           />
         ))}
+        <Grid container>
+          <UploadThumbnails thumbnails={thumbnails} />
+          <Button
+            onClick={() => fileUpload(setThumbnails, 5 - thumbnails.length)}
+          >
+            Upload
+          </Button>
+        </Grid>
       </AddReviewModal>
     </Grid>
   );
